56112e469fd7b69838fcd8a7cebce870cd9da026
Александр <alexandr31233@gmail.com>
2024-03-16 00:25:46 +0300

    Поправил

--- Mission_4.py
+++ Mission_4.py
@@ -1,20 +1,15 @@
 def check_brackets_balance(file_path):
     stack = []
-    opening_brackets = "("  # Открывающие скобки
-    closing_brackets = ")"  # Закрывающие скобки
 
     with open(file_path, 'r') as file:
         for line in file:
             for char in line:
-                if char in opening_brackets:
-                    stack.append(char)
-                elif char in closing_brackets:
+                if char == '(':
+                    stack.append('(')
+                elif char == ')':
                     if not stack:  # Если стек пустой, а закрывающая скобка встретилась
                         return False
-                    if opening_brackets.index(stack[-1]) == closing_brackets.index(char):
-                        stack.pop()
-                    else:
-                        return False
+                    stack.pop()
 
     return len(stack) == 0
 
